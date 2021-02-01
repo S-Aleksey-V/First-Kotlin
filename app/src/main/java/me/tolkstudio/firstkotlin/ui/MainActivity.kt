9aa2c2e06dfd8a67cd2575f5d1dbc7cc0a46b8bb
Alexey <gamestol@mail.ru>
2021-02-01 23:12:44 +0300

    add Logout

--- app/src/main/java/me/tolkstudio/firstkotlin/ui/MainActivity.kt
+++ app/src/main/java/me/tolkstudio/firstkotlin/ui/MainActivity.kt
@@ -3,7 +3,11 @@ package me.tolkstudio.firstkotlin.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
+import com.firebase.ui.auth.AuthUI
 import kotlinx.android.synthetic.main.activity_main.*
 import me.tolkstudio.firstkotlin.R
 import me.tolkstudio.firstkotlin.databinding.ActivityMainBinding
@@ -11,7 +15,7 @@ import me.tolkstudio.firstkotlin.model.Note
 import me.tolkstudio.firstkotlin.viewmodel.MainViewModel
 
 
-class MainActivity : BaseActivity<List<Note>?, MainViewState>() {
+class MainActivity : BaseActivity<List<Note>?, MainViewState>(), LogoutDialog.LogoutListener {
 
     override val viewModel: MainViewModel
             by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
@@ -43,6 +47,20 @@ class MainActivity : BaseActivity<List<Note>?, MainViewState>() {
         adapter.notes = data
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean =
+            MenuInflater(this).inflate(R.menu.bottomappbar, menu).let { true }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+            when (item.itemId) {
+                R.id.logout -> showLogoutDialog().let { true }
+                else -> false
+            }
+
+    private fun showLogoutDialog() {
+        supportFragmentManager.findFragmentByTag(LogoutDialog.TAG)
+                ?: LogoutDialog.createInstance().show(supportFragmentManager, LogoutDialog.TAG)
+    }
+
     private fun openNoteScreen(note: Note?) {
         val intent = NoteActivity.getStartIntent(this, note?.id)
         startActivity(intent)
@@ -50,5 +68,14 @@ class MainActivity : BaseActivity<List<Note>?, MainViewState>() {
 
     companion object {
         fun getStartIntent(context: Context) = Intent(context, MainActivity::class.java)
+    }
+
+    override fun onLogout() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    startActivity(Intent(this, SplashActivity::class.java))
+                    finish()
+                }
     }
 }
